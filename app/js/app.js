6f03f366be5d8e782545396d6b1b962622a3c492
Marish04 <94753536+Marish04@users.noreply.github.com>
2022-04-27 14:47:29 -0700

    add section header

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,17 @@
-$(document).ready(function () {});
+const dropdown = document.querySelector('.dropdown');
+const dropdownUl = document.querySelector('.dropdown-ul');
+const burger = document.querySelector('.burger');
+const navUl =document.querySelector('.nav-ul')
+
+
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('open');
+    dropdownUl.classList.toggle('open');
+})
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('open');
+    navUl.classList.toggle('open')
+})
+
+
